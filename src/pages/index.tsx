7c320bd94f4d004363108ece26b0c6692ed27e69
edumartinsg <edu19_96@hotmail.com>
2021-05-11 10:33:12 -0300

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Início | igNews</title>
+      </Head>
+      <h1>Hello world</h1>
+    </>
+  );
+}
