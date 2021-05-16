757e784a408dae9f644718f772c0197e169fd003
edumartinsg <edu19_96@hotmail.com>
2021-05-16 18:05:09 -0300

    static side gen

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 import Head from "next/head";
 import { SubscribeButton } from "../components/SubscribeButton";
 import { stripe } from "../services/stripe";
@@ -37,7 +37,7 @@ export default function Home({ product }: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getServerSideProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve("price_1IqNnxFNdhu2vSjFwBl8CYmg", {
     expand: ["product"],
   });
@@ -54,5 +54,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
     props: {
       product,
     },
+    revalidate: 60 * 60 * 24, //24 hours
   };
 };
